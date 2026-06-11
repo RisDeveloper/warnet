94c7af4c913900521cbfd2a942aab48fc0f08815
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:18 +0700

    Add operator/hapus_member.php

---
+++ operator/hapus_member.php
@@ -0,0 +1,20 @@
+﻿<?php
+include "../koneksi.php";
+
+$id = $_GET['id'];
+
+db_query($conn,
+    "DELETE FROM transaksi WHERE id_member='$id'"
+);
+
+db_query($conn,
+    "DELETE FROM member WHERE id_member='$id'"
+);
+
+echo "
+<script>
+alert('Data berhasil dihapus');
+window.location='member.php';
+</script>
+";
+?>
