53f10e37a70ee62a9d37980afa2d40fbaa06435a
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:10:54 +0700

    Add login_operator.php

---
+++ login_operator.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Operator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Login Operator</h2>
+
+<form action="proses_operator.php" method="POST">
+    Username :
+    <input type="text" name="username" required>
+    <br><br>
+
+    Password :
+    <input type="password" name="password" required>
+    <br><br>
+
+    <button type="submit">Login</button>
+</form>
+
+<br>
+<a href="login_member.php">Login Sebagai Member</a>
+
+</body>
+</html>
