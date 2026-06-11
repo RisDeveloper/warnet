0be7004b750f497304a78991da5b205ef2f6d59d
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:13 +0700

    Add operator/edit_komputer.php

---
+++ operator/edit_komputer.php
@@ -0,0 +1,64 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$id = $_GET['id'];
+
+$data = db_query(
+$conn,
+"SELECT * FROM komputer
+WHERE id_pc='$id'"
+);
+
+$d = db_fetch_assoc($data);
+
+?>
+
+<h2>Edit Komputer</h2>
+
+<form action="update_komputer.php" method="POST">
+
+<input
+type="hidden"
+name="id_pc"
+value="<?php echo $d['id_pc']; ?>"
+>
+
+Nama PC :
+
+<input
+type="text"
+name="nama_pc"
+value="<?php echo $d['nama_pc']; ?>"
+>
+
+<br><br>
+
+Status :
+
+<select name="status">
+
+<option value="Tersedia">Tersedia</option>
+
+<option value="Dipakai">Dipakai</option>
+
+</select>
+
+<br><br>
+
+<button type="submit">
+Update
+</button>
+<a href="komputer.php">
+Kembali
+</a>
+</form>
