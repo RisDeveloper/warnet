dff3a5d9277d76e0fd0326a8a94174ea433467c6
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:05 +0700

    Add member/dashboard.php

---
+++ member/dashboard.php
@@ -0,0 +1,36 @@
+﻿<?php
+session_start();
+include "../koneksi.php";
+
+if(!isset($_SESSION['id_member'])){
+    header("Location: ../login_member.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard Member</title>
+<link rel="stylesheet" href="../style.css">
+</head>
+<body>
+
+<h1>Dashboard Member</h1>
+
+<p>
+Selamat Datang
+<?php echo $_SESSION['nama']; ?>
+</p>
+
+<hr>
+
+<div class="menu">
+
+<a href="paket.php">Pilih Paket</a>
+<a href="history.php">History</a>
+<a href="../logout.php">Logout</a>
+
+</div>
+
+</body>
+</html>
