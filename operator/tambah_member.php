cce3abef7479c7823b3e5e259a71219f40b7f16b
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:33 +0700

    Add operator/tambah_member.php

---
+++ operator/tambah_member.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<h2>Tambah Member</h2>
+
+<form action="simpan_member.php" method="POST">
+
+Nama :
+
+<input type="text" name="nama">
+
+<br><br>
+
+Username :
+
+<input type="text" name="username">
+
+<br><br>
+
+Password :
+
+<input type="text" name="password">
+
+<br><br>
+
+Saldo :
+
+<input type="number" name="saldo">
+
+<br><br>
+
+<button type="submit">
+Simpan
+</button>
+<a href="member.php">
+Kembali
+</a>
+</form>
