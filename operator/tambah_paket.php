9b83e6e60996314b6251ff106d2a773259faf2eb
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:34 +0700

    Add operator/tambah_paket.php

---
+++ operator/tambah_paket.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<h2>Tambah Paket</h2>
+
+<form action="simpan_paket.php" method="POST">
+
+Nama Paket :
+
+<input type="text" name="nama_paket">
+
+<br><br>
+
+Durasi (Jam) :
+
+<input type="number" name="durasi_jam">
+
+<br><br>
+
+Harga :
+
+<input type="number" name="harga">
+
+<br><br>
+
+<button type="submit">
+Simpan
+</button>
+<a href="paket.php">
+Kembali
+</a>
+</form>
