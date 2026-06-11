b7fb2abe8331fbc5f23763e28628ab722ed70019
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:10:57 +0700

    Add proses_member.php

---
+++ proses_member.php
@@ -0,0 +1,22 @@
+﻿<?php
+session_start();
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = db_query($conn,
+    "SELECT * FROM member WHERE username='$username' AND password='$password'"
+);
+
+$data = db_fetch_assoc($query);
+
+if($data){
+    $_SESSION['id_member'] = $data['id_member'];
+    $_SESSION['nama'] = $data['nama'];
+    header("Location: member/dashboard.php");
+    exit;
+}else{
+    echo "Login Gagal";
+}
+?>
