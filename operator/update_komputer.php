57e3c13ae4878eb250f6b1857a55a6cc98387d61
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:37 +0700

    Add operator/update_komputer.php

---
+++ operator/update_komputer.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$id = $_POST['id_pc'];
+
+$nama_pc = $_POST['nama_pc'];
+
+$status = $_POST['status'];
+
+db_query($conn,
+
+"UPDATE komputer SET
+
+nama_pc='$nama_pc',
+status='$status'
+
+WHERE id_pc='$id'"
+
+);
+
+header("Location: komputer.php");
