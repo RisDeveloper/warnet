6414cfbb74f69a0cd0a9e7b730ad8c29c9944764
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:10:51 +0700

    Add koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,51 @@
+﻿<?php
+$host = "db.oaypsziqktajxtbrqlww.supabase.co";
+$port = "5432";
+$dbname = "postgres";
+$user = "postgres";
+$password = "yOo780kiPnwvW9ok";
+
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if(!$conn){
+    die("Koneksi gagal: " . pg_last_error());
+}
+
+function db_query($conn, $query) {
+    return pg_query($conn, $query);
+}
+
+function db_fetch_assoc($result) {
+    return pg_fetch_assoc($result);
+}
+
+function db_fetch_array($result) {
+    return pg_fetch_array($result);
+}
+
+function db_num_rows($result) {
+    return pg_num_rows($result);
+}
+
+function db_affected_rows($conn) {
+    return pg_affected_rows($conn);
+}
+
+function db_escape_string($conn, $str) {
+    return pg_escape_string($conn, $str);
+}
+
+function db_error($conn) {
+    return pg_last_error($conn);
+}
+
+function db_insert_id($conn, $sequence) {
+    $res = pg_query($conn, "SELECT lastval()");
+    $row = pg_fetch_row($res);
+    return $row[0] ?? 0;
+}
+
+function db_connect($host, $user, $password, $dbname, $port = 5432) {
+    return pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+}
+?>
