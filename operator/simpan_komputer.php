a22c57e6d1746848fad79b4e3e56fd8153fc7f83
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:28 +0700

    Add operator/simpan_komputer.php

---
+++ operator/simpan_komputer.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$nama_pc = $_POST['nama_pc'];
+
+db_query($conn,
+
+"INSERT INTO komputer
+(nama_pc,status)
+
+VALUES
+
+('$nama_pc','Tersedia')"
+
+);
+
+header("Location: komputer.php");
