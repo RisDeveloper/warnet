499b743e82a6b8c7ebb9955b87f718abcca67450
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:28:54 +0700

    Add error reporting to koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,7 @@
 ﻿<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $host = getenv("PGHOST") ?: "db.oaypsziqktajxtbrqlww.supabase.co";
 $port = getenv("PGPORT") ?: "5432";
 $dbname = getenv("PGDATABASE") ?: "postgres";
