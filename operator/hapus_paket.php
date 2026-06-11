7477a847a240fa91e43c514881f9dd42fcabbf41
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:19 +0700

    Add operator/hapus_paket.php

---
+++ operator/hapus_paket.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$id = $_GET['id'];
+
+db_query(
+$conn,
+"DELETE FROM paket
+WHERE id_paket='$id'"
+);
+
+header("Location: paket.php");
