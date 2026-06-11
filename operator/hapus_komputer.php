e2f9cf4b225d13353b42b680da097e63bcbe5f0e
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:16 +0700

    Add operator/hapus_komputer.php

---
+++ operator/hapus_komputer.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$id = $_GET['id'];
+
+db_query(
+$conn,
+"DELETE FROM komputer
+WHERE id_pc='$id'"
+);
+
+header("Location: komputer.php");
