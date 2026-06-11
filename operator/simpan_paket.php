ef813a3cfa82ad9c0e24efe4ae3048034129ddac
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:30 +0700

    Add operator/simpan_paket.php

---
+++ operator/simpan_paket.php
@@ -0,0 +1,31 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$nama_paket = $_POST['nama_paket'];
+$durasi_jam = $_POST['durasi_jam'];
+$harga = $_POST['harga'];
+
+db_query($conn,
+
+"INSERT INTO paket
+(nama_paket,durasi_jam,harga)
+
+VALUES
+
+('$nama_paket',
+'$durasi_jam',
+'$harga')
+
+");
+
+header("Location: paket.php");
