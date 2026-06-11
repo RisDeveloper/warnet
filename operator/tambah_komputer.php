6ff2f2cb857c9d40ab04f1c8698d9ce7e3278edd
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:32 +0700

    Add operator/tambah_komputer.php

---
+++ operator/tambah_komputer.php
@@ -0,0 +1,26 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<h2>Tambah Komputer</h2>
+
+<form action="simpan_komputer.php" method="POST">
+
+Nama PC :
+
+<input type="text" name="nama_pc">
+
+<br><br>
+
+<button type="submit">
+Simpan
+</button>
+<a href="komputer.php">
+Kembali
+</a>
+</form>
