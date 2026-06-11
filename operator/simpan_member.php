60a12e26dffa007c19a7c4d086302966790f4276
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:29 +0700

    Add operator/simpan_member.php

---
+++ operator/simpan_member.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$nama = $_POST['nama'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$saldo = $_POST['saldo'];
+
+db_query($conn,
+
+"INSERT INTO member
+(nama,username,password,saldo)
+
+VALUES
+
+('$nama',
+'$username',
+'$password',
+'$saldo')
+
+");
+
+header("Location: member.php");
