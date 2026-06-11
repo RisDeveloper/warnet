7a98da7c644337b4981b28d14ef2d7162e561c6a
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:10:59 +0700

    Add proses_operator.php

---
+++ proses_operator.php
@@ -0,0 +1,22 @@
+﻿<?php
+session_start();
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = db_query($conn,
+    "SELECT * FROM operator WHERE username='$username' AND password='$password'"
+);
+
+$data = db_fetch_assoc($query);
+
+if($data){
+    $_SESSION['id_operator'] = $data['id_operator'];
+    $_SESSION['nama'] = $data['nama'];
+    header("Location: operator/dashboard.php");
+    exit;
+}else{
+    echo "Login Gagal";
+}
+?>
