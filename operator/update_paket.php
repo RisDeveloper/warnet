a2e609c2f7149cb94dc20419181bb9bbcfc14ad7
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:40 +0700

    Add operator/update_paket.php

---
+++ operator/update_paket.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$id = $_POST['id_paket'];
+
+$nama_paket = $_POST['nama_paket'];
+$durasi_jam = $_POST['durasi_jam'];
+$harga = $_POST['harga'];
+
+db_query($conn,
+
+"UPDATE paket SET
+
+nama_paket='$nama_paket',
+durasi_jam='$durasi_jam',
+harga='$harga'
+
+WHERE id_paket='$id'"
+
+);
+
+header("Location: paket.php");
