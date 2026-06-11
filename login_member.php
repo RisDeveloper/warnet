219663d49a64fb412ac5177fb027cc4c2e539a50
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:10:52 +0700

    Add login_member.php

---
+++ login_member.php
@@ -0,0 +1,38 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Member</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h2>Selamat Datang DI Warnet AKU GW</h2>
+
+<h3>Login Member</h3>
+
+<form action="proses_member.php" method="POST">
+
+Username :
+<input type="text" name="username">
+
+<br><br>
+
+Password :
+<input type="password" name="password">
+
+<br><br>
+
+<button type="submit">
+Login
+</button>
+
+</form>
+
+<br>
+
+<a href="login_operator.php">
+Login Sebagai Operator
+</a>
+
+</body>
+</html>
