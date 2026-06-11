46a8ad19e91f40fc0a64556e95ab9869aebb525e
Ris Dev <140927214+RisDeveloper@users.noreply.github.com>
2026-06-11 15:11:38 +0700

    Add operator/update_member.php

---
+++ operator/update_member.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+
+
+<link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+<?php
+
+include "../koneksi.php";
+
+$id = $_POST['id_member'];
+$nama = $_POST['nama'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$saldo = $_POST['saldo'];
+
+db_query($conn,
+
+"UPDATE member SET
+
+nama='$nama',
+username='$username',
+password='$password',
+saldo='$saldo'
+
+WHERE id_member='$id'"
+
+);
+
+header("Location: member.php");
